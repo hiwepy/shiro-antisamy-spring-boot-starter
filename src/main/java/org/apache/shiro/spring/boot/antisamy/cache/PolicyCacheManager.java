c1880ffeb8e3de0b6f3bcd14a8b56ae6b39154a1
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:07:00 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/antisamy/cache/PolicyCacheManager.java
+++ src/main/java/org/apache/shiro/spring/boot/antisamy/cache/PolicyCacheManager.java
@@ -15,7 +15,7 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 
 /**
  * Policy对象缓存管理
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class PolicyCacheManager {
 	
