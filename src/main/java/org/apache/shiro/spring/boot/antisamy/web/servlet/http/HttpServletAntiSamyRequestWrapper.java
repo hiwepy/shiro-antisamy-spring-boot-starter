ee1170c7b330585e375d9649090402368d3299a3
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:07:06 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/antisamy/web/servlet/http/HttpServletAntiSamyRequestWrapper.java
+++ src/main/java/org/apache/shiro/spring/boot/antisamy/web/servlet/http/HttpServletAntiSamyRequestWrapper.java
@@ -15,7 +15,7 @@ import org.apache.shiro.spring.boot.antisamy.utils.XssScanUtils;
 
 /**
  * RichText XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class HttpServletAntiSamyRequestWrapper extends HttpServletRequestWrapper {
 
